79bed3968061d758db02b8f445125f16022bcae9
Prelang Builder <builder@prelang.com>
2014-11-25 21:58:24 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,7 +3,7 @@ class MicropostsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_micropost, only: [:show, :edit, :update, :destroy]
+  before_action :set_micropost, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /microposts
   # GET /microposts.json
